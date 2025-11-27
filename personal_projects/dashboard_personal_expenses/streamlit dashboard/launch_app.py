2e9b66c7a2f9c020b2941a9055e458eb5c5a8192
claudiofcosta <claudiofcosta@live.com.pt>
2025-11-27 16:17:37 +0100

    removed files due to wrong path

--- personal_projects/dashboard_personal_expenses/streamlit dashboard/launch_app.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-# Launches your Streamlit app inside the .exe
-app_path = os.path.join(os.path.dirname(sys.argv[0]), "streamlit dashboard\dashboard.py")
-os.system(f'streamlit run "{app_path}"')
