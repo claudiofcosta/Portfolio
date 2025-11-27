93577947fb0fbc454b5a90731a560918fae51bb3
claudiofcosta <claudiofcosta@live.com.pt>
2025-11-27 16:10:52 +0100

    fixed a file path in launch_app.py

--- personal_projects/dashboard_personal_expenses/streamlit dashboard/launch_app.py
+++ personal_projects/dashboard_personal_expenses/streamlit dashboard/launch_app.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 # Launches your Streamlit app inside the .exe
-app_path = os.path.join(os.path.dirname(sys.argv[0]), "streamlit_dashboard\dashboard.py")
+app_path = os.path.join(os.path.dirname(sys.argv[0]), "streamlit dashboard\dashboard.py")
 os.system(f'streamlit run "{app_path}"')
